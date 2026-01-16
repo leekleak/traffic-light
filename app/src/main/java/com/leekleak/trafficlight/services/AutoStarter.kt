eb83c1686853b4f7c693e7299556194fb254e6fd
leekleak <leekleak@proton.me>
2026-01-16 18:25:17 +0100

    Fix notification not restarting on boott

--- app/src/main/java/com/leekleak/trafficlight/services/AutoStarter.kt
+++ app/src/main/java/com/leekleak/trafficlight/services/AutoStarter.kt
@@ -3,12 +3,17 @@ package com.leekleak.trafficlight.services
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import kotlin.getValue
 
-class AutoStarter : BroadcastReceiver()
+class AutoStarter : BroadcastReceiver(), KoinComponent
 {
+    val permissionManager: PermissionManager by inject()
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
             context?.let {
+                permissionManager.update()
                 UsageService.startService(it)
             }
         }
