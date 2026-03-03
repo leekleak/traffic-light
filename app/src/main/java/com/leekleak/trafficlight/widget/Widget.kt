1bb1d341ec8bee0fa00f120bde2b714d32f29859
Vytautas Butėnas <leekleak@proton.me>
2026-03-03 11:32:27 +0100

    Fix widget being cutoff

--- app/src/main/java/com/leekleak/trafficlight/widget/Widget.kt
+++ app/src/main/java/com/leekleak/trafficlight/widget/Widget.kt
@@ -30,7 +30,6 @@ import androidx.glance.layout.Box
 import androidx.glance.layout.Column
 import androidx.glance.layout.ContentScale
 import androidx.glance.layout.Row
-import androidx.glance.layout.fillMaxHeight
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.height
@@ -181,62 +180,63 @@ class Widget: GlanceAppWidget() {
                         colorFilter = ColorFilter.tint(GlanceTheme.colors.primaryContainer)
                     )
                 }
-                Column(GlanceModifier.fillMaxHeight()) {
-                    Row(GlanceModifier.padding(16.dp).fillMaxWidth()) {
-                        SimIcon(simNumber)
-                        Text(
-                            modifier = GlanceModifier.fillMaxWidth().defaultWeight(),
-                            text = carrierName,
-                            style = TextStyle(
-                                color = GlanceTheme.colors.onSurface,
-                                textAlign = TextAlign.End
-                            )
+                Row(GlanceModifier.padding(16.dp).fillMaxWidth()) {
+                    SimIcon(simNumber)
+                    Text(
+                        modifier = GlanceModifier.fillMaxWidth().defaultWeight(),
+                        text = carrierName,
+                        style = TextStyle(
+                            color = GlanceTheme.colors.onSurface,
+                            textAlign = TextAlign.End
                         )
-                    }
-                    Column(
-                        modifier = GlanceModifier.fillMaxSize().defaultWeight(),
-                        verticalAlignment = Alignment.CenterVertically
+                    )
+                }
+                Column(
+                    modifier = GlanceModifier.fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Row(
+                        modifier = GlanceModifier.padding(bottom = 8.dp),
+                        verticalAlignment = Alignment.Bottom,
                     ) {
-                        Row(
-                            modifier = GlanceModifier.fillMaxWidth(),
-                            verticalAlignment = Alignment.Bottom,
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Text(
-                                text = usageString,
-                                style = TextStyle(
-                                    color = GlanceTheme.colors.onSurface,
-                                    fontSize = 64.sp
-                                ),
-                            )
-                            Text(
-                                text = "/${quotaString}GB",
-                                style = TextStyle(
-                                    color = GlanceTheme.colors.onSurface,
-                                    fontSize = 36.sp,
-                                ),
-                            )
-                        }
-                    }
-                    Column(GlanceModifier.padding(16.dp)) {
                         Text(
-                            modifier = GlanceModifier.fillMaxWidth().padding(bottom = 8.dp),
-                            text = resetString,
+                            text = usageString,
                             style = TextStyle(
                                 color = GlanceTheme.colors.onSurface,
-                                fontSize = 16.sp,
-                                textAlign = TextAlign.Center
+                                fontSize = 64.sp,
                             ),
                         )
-                        LinearProgressIndicator(
-                            modifier = GlanceModifier
-                                .height(4.dp)
-                                .fillMaxWidth(),
-                            color = GlanceTheme.colors.primary,
-                            backgroundColor = GlanceTheme.colors.primaryContainer,
-                            progress = progress.toFloat(),
+                        Text(
+                            text = "/${quotaString}GB",
+                            style = TextStyle(
+                                color = GlanceTheme.colors.onSurface,
+                                fontSize = 36.sp,
+                            ),
                         )
                     }
+                }
+                Column(
+                    modifier = GlanceModifier.fillMaxSize().padding(16.dp),
+                    verticalAlignment = Alignment.Bottom,
+                ) {
+                    Text(
+                        modifier = GlanceModifier.fillMaxWidth().padding(bottom = 8.dp),
+                        text = resetString,
+                        style = TextStyle(
+                            color = GlanceTheme.colors.onSurface,
+                            fontSize = 16.sp,
+                            textAlign = TextAlign.Center
+                        ),
+                    )
+                    LinearProgressIndicator(
+                        modifier = GlanceModifier
+                            .height(4.dp)
+                            .fillMaxWidth(),
+                        color = GlanceTheme.colors.primary,
+                        backgroundColor = GlanceTheme.colors.primaryContainer,
+                        progress = progress.toFloat(),
+                    )
                 }
             }
         }
