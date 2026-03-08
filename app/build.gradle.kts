b6475fde50ed91236e194360c6f0459d75efa8a6
Vytautas Butėnas <leekleak@proton.me>
2026-03-08 13:50:44 +0100

    Add changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.leekleak.trafficlight"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "2.11"
+        versionCode = 20
+        versionName = "2.11.1"
         base.archivesName = "$namespace-$versionName"
     }
     buildTypes {
