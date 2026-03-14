7307e6071e9ad2e551b6f11b01a2fff3d99f003d
Vytautas Butėnas <leekleak@proton.me>
2026-03-14 23:47:29 +0100

    Run service async on boot

--- app/src/main/java/com/leekleak/trafficlight/services/AutoStarter.kt
+++ app/src/main/java/com/leekleak/trafficlight/services/AutoStarter.kt
@@ -4,18 +4,26 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.leekleak.trafficlight.widget.startAlarmManager
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-class AutoStarter : BroadcastReceiver(), KoinComponent
-{
+class AutoStarter : BroadcastReceiver(), KoinComponent {
     private val permissionManager: PermissionManager by inject()
-    override fun onReceive(context: Context?, intent: Intent?) {
-        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
-            context?.let {
-                permissionManager.update()
-                UsageService.startService(it)
-                startAlarmManager(context)
+    @OptIn(DelicateCoroutinesApi::class)
+    override fun onReceive(context: Context, intent: Intent) {
+        val pendingResult = goAsync()
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            GlobalScope.launch(Dispatchers.IO) {
+                context.let {
+                    permissionManager.update()
+                    UsageService.startService(it)
+                    startAlarmManager(context)
+                }
+                pendingResult.finish()
             }
         }
     }
