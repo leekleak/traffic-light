57d9fea44b9dd019369ba0adfdfa2ab74efc8c8a
Vytautas Butėnas <leekleak@proton.me>
2026-03-16 12:05:51 +0100

    Catch SecurityException

--- app/src/main/java/com/leekleak/trafficlight/model/AppManager.kt
+++ app/src/main/java/com/leekleak/trafficlight/model/AppManager.kt
@@ -72,7 +72,12 @@ class AppManager(
         return when (uid) {
             UID_TETHERING -> listOf("")
             UID_REMOVED -> listOf("")
-            else -> packageManager.getPackagesForUid(uid)?.toList()
+            else -> try {
+                packageManager.getPackagesForUid(uid)?.toList()
+            } catch (e: Exception) {
+                Timber.w(e, "Failed to get package names for UID $uid")
+                null
+            }
         }
     }
 
