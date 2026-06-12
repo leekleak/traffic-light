1a225176badb3c2b1b0af0fff8a17dab8a4fd260
Vytautas Butėnas <leekleak@proton.me>
2026-06-12 19:05:47 +0200

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.leekleak.trafficlight"
         minSdk = 28
         targetSdk = 37
-        versionCode = 39
-        versionName = "3.0-alpha1"
+        versionCode = 40
+        versionName = "3.0-alpha2"
         base.archivesName = "$namespace-$versionName"
 
         manifestPlaceholders["admobAppId"] = "ca-app-pub-3940256099942544~3347511713"
